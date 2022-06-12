aa0f70e1cf4cc2c81f694aca2561d04e569eab28
Fang Yue <yuefang323@gmail.com>
2022-06-11 23:10:31 -0700

    add error messages at api routes

--- app/forms/EditAnswerForm.py
+++ app/forms/EditAnswerForm.py
@@ -3,6 +3,6 @@ from wtforms import IntegerField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
 class EditAnswerForm(FlaskForm):
-    answer = TextAreaField("answer", validators=[DataRequired(), Length(min=1, max=5000, message='Answer content must be between 1 and 5000 characters.')])
+    answer = TextAreaField("answer", validators=[DataRequired(message='Please provide your answer.'), Length(min=3, max=5000, message='Answer content must be between 3 and 5000 characters.')])
     user_id = IntegerField("tag_id")
     question_id = IntegerField("question_id")
--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,3 +2,5 @@ from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .NewQuestionForm import NewQuestionForm
 from .EditQuestionForm import EditQuestionForm
+from .NewAnswerForm import NewAnswerForm
+from .EditAnswerForm import EditAnswerForm
