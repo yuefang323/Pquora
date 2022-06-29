c87344a079f7efcc1ab22be2f1547b2ea026fc6d
Fang Yue <yuefang323@gmail.com>
2022-06-29 11:01:17 -0700

    add question component

---
+++ react-app/src/components/Profile/UserQuestions.js
@@ -0,0 +1,39 @@
+
+import { NavLink } from "react-router-dom";
+
+import CreatedAt from "../util/CreatedAt";
+import UpdatedAt from "../util/UpdatedAt";
+
+function UserQuestions({ setShow, arr }) {
+    return (
+        <>
+        {arr.map((obj) => (
+                    <div
+                        key={"userquestion" + obj.id}
+                        className="home-question-item"
+                    >
+                        <NavLink
+                            to={`/questions/${obj.id}`}
+                            exact={true}
+                            className="question-detail"
+                        >
+                            <div className="question-detail">
+                                <p className="question-detail">{obj.content}</p>
+                            </div>
+                        </NavLink>
+                        <div className="time">
+                            <div className="create-at-time">
+                                Created at:{" "}
+                                <CreatedAt created_at={obj.created_at} />
+                            </div>
+                            <div className="update-at-time">
+                                Updated at:{" "}
+                                <UpdatedAt updated_at={obj.updated_at} />
+                            </div>
+                        </div>
+                    </div>
+                ))}
+                </>
+    )
+}
+export default UserQuestions;
--- react-app/src/components/Profile/index.js
+++ react-app/src/components/Profile/index.js
@@ -1,14 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Redirect, NavLink } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
 import NavBar from "../NavBar";
+import { useSearch } from "../../context/Query";
+import UserQuestions from "./UserQuestions";
+import UserAnswers from "./UserAnswers";
 
 function User() {
     const user = useSelector((state) => state.session.user);
     const userId = user.id;
     const [questions, setQuestions] = useState([]);
+    const questionsOrdered = questions.sort((a, b) =>
+        b.updated_at.localeCompare(a.updated_at)
+    );
     const [answers, setAnswers] = useState([]);
+    const answersOrdered = answers.sort((a, b) =>
+        b.updated_at.localeCompare(a.updated_at)
+    );
+    const { search } = useSearch();
+    const [arr, setArr] = useState([]);
+    const [aarr, setAarr] = useState([]);
+
+    const [show, setShow] = useState(false);
 
     useEffect(() => {
         async function fetchData() {
@@ -19,8 +33,22 @@ function User() {
         }
         fetchData();
     }, [userId]);
-    console.log(questions, "......")
-    console.log(answers, "xxxxxxxxxx")
+    console.log(questions, "......");
+    console.log(answers, "xxxxxxxxxx");
+
+    useEffect(() => {
+        let newArr = questionsOrdered.filter((question) =>
+            question.content.toLowerCase().includes(search.toLowerCase().trim())
+        );
+        setArr(newArr);
+    }, [search, questions]);
+
+    useEffect(() => {
+        let newAarr = answersOrdered.filter((answer) =>
+            answer.content.toLowerCase().includes(search.toLowerCase().trim())
+        );
+        setAarr(newAarr);
+    }, [search, answers]);
 
     if (!user) return <Redirect to="/" />;
 
@@ -30,35 +58,16 @@ function User() {
             <div className="questions-list-content">
                 <div className="question-box">
                     <p>Hi, {user.username}! You have</p>
-                    <h2 className="box-word">
-                        {questions.length} questions | {answers.length} answers
+                    <h2 className="box-word" onClick={() => setShow(true)}>
+                        {questions.length} questions
+                        {/* {questions.length} questions | {answers.length} answers */}
+                    </h2>
+                    <h2 className="box-word" onClick={() => setShow(true)}>
+                        {answers.length} answers{" "}
                     </h2>
                 </div>
-                <ul>
-                    <li>
-                        <strong>User Id</strong> {userId}
-                    </li>
-                    <li>
-                        <strong>Username</strong> {user.username}
-                    </li>
-                    <li>
-                        <strong>Email</strong> {user.email}
-                    </li>
-                </ul>
-                {questions.map((obj) => (
-                    <div>
-                        <NavLink
-                            to={`/questions/${obj.id}`}
-                            exact={true}
-                            className="question-detail"
-                        >
-                            <div className="question-detail">
-                                <p className="question-detail">{obj.content}</p>
-                            </div>
-                        </NavLink>
-
-                    </div>
-                ))}
+                {show && <UserQuestions arr={arr} setShow={setShow} />}
+                {show && <UserAnswers aarr={aarr} setShow={setShow} />}
             </div>
         </div>
     );
