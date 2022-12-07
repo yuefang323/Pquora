5c89f688f064600c9d700b0d25b0e9799c191e8a
Fang Yue <yuefang323@gmail.com>
2022-12-07 10:56:29 -0800

    adjust seed commands to use schema in production

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -3,6 +3,8 @@ from .users import seed_users, undo_users
 from .questions import seed_questions, undo_questions
 from .answers import seed_answers, undo_answers
 from .tags import seed_tags, undo_tags 
+# ...for deploy in render
+from app.models.db import db, environment, SCHEMA
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -12,6 +14,12 @@ seed_commands = AppGroup('seed')
 # Creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
+    if environment == 'production':
+        # Before seeding, truncate all tables prefixed with schema name
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        # Add a truncate command here for every table that will be seeded.
+        db.session.commit()
+        
     seed_users()
     seed_tags()
     seed_questions()
